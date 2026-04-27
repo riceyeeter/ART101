dce77d01f7966b896749bd9b96695785a06f7600
riceyeeter <dxchang@ucsc.edu>
2026-04-26 19:00:28 -0700

    Progress since last time

---
+++ Exercises/ex04.js
@@ -0,0 +1,25 @@
+
+
+let count = 0;
+
+let colors= ["Orchid", "Coral", "HotPink", "Plum"];
+
+
+
+$("#important-button").click( function(){
+
+$("#important-button").html( "Clicks " + count + " " + colors[count] );
+
+count=count+1;
+
+$("html").css("background-color", colors [count]);
+
+console.log (pencil.color));
+
+
+});
+
+
+
+
+
